018ef66f625d0d8003c20a18a60cd609795aea06
Joe Essig <essigjnp@gmail.com>
2018-09-13 09:56:37 -0500

    Try again...

--- app/templatetags/app_tags.py
+++ app/templatetags/app_tags.py
@@ -11,11 +11,15 @@ def get_events():
 
 @register.simple_tag()
 def get_verse():
-	return Verse.objects.filter(active=True)[0]
+	verses = Verse.objects.filter(active=True)
+	verses = verses[0]
+	return 
 
 @register.simple_tag()
 def get_featured_events():
-	return Event.objects.filter(featured=True).order_by('datetime')[0]
+	events = Event.objects.filter(featured=True).order_by('datetime')
+	events = events[0]
+	return events
 
 @register.simple_tag()
 def get_missions(category):
